d7a7dcd879a2cb3dc0e43816c7af150bd19eb215
danish-mehmood <rdm355190@gmail.com>
2025-04-23 07:34:50 +0500

    done with CMstate

---
+++ raft.go
@@ -0,0 +1,32 @@
+// Core Raft implementation - Consensus Module.
+
+const DebugCM = 1
+
+type LogEntry struct {
+	Command any
+	Term    int
+}
+
+type CMState int
+
+const (
+	Follower CMState = iota
+	Candidate
+	Leader
+	Dead
+)
+
+func (s CMState) String() string {
+	switch s {
+	case Follower:
+		return "Follower"
+	case Candidate:
+		return "Candidate"
+	case Leader:
+		return "Leader"
+	case Dead:
+		return "Dead"
+	default:
+		panic("unreachable")
+	}
+}
