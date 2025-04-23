b20f40ecafed33f2f92207d001ecfa0482042e7e
danish-mehmood <rdm355190@gmail.com>
2025-04-23 07:38:54 +0500

    done with cm struct

--- raft.go
+++ raft.go
@@ -30,3 +30,42 @@ func (s CMState) String() string {
 		panic("unreachable")
 	}
 }
+
+// ConsensusModule (CM) implements a single node of Raft consensus.
+type ConsensusModule struct{
+	mu sync.Mutex
+	id int
+	peerIds []int
+	server *Server 
+	currentTerm int 
+	votedFor int
+	log []LogEntry
+
+	state CMState
+	electionResetEvent time.time
+}
+
+
+// NewConsensusModule creates a new CM with the given ID, list of peer IDs and
+// server. The ready channel signals the CM that all peers are connected and
+// it's safe to start its state machine.
+func NewConsensusModule(id int , peerIds []int , server *Server , ready <-chan any)*ConsensusModule{
+	cm := new(ConsensusModule)
+	cm.id =id 
+	cm.peerIds = peerIds
+	cm.server = server
+	cm.state = Follower
+	cm.votedFor = -1
+
+	o func() {
+		// The CM is quiescent until ready is signaled; then, it starts a countdown
+		// for leader election.
+		<-ready
+		cm.mu.Lock()
+		cm.electionResetEvent = time.Now()
+		cm.mu.Unlock()
+		cm.runElectionTimer()
+	}()
+
+	return cm
+}
